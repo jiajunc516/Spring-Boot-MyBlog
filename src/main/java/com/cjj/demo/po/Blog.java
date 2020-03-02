455e85908e8a269cabd7c1efe1612bedae708e89
jiajunchang <jiajunc@uci.edu>
2020-03-02 01:09:46 -0800

    Add Blog.java

---
+++ src/main/java/com/cjj/demo/po/Blog.java
@@ -0,0 +1,102 @@
+package com.cjj.demo.po;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "t_blog")
+public class Blog {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String content;
+    private String firstPicture;
+    private String flag;
+    private Integer views;
+    private boolean support;
+    private boolean shareStatement;
+    private boolean commentable;
+    private boolean published;
+    private boolean recommend;
+    private Date createTime;
+    private Date updateTime;
+
+    public Blog() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getFirstPicture() {
+        return firstPicture;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public Integer getViews() {
+        return views;
+    }
+
+    public boolean isSupport() {
+        return support;
+    }
+
+    public boolean isShareStatement() {
+        return shareStatement;
+    }
+
+    public boolean isCommentable() {
+        return commentable;
+    }
+
+    public boolean isPublished() {
+        return published;
+    }
+
+    public boolean isRecommend() {
+        return recommend;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Blog{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", firstPicture='" + firstPicture + '\'' +
+                ", flag='" + flag + '\'' +
+                ", views=" + views +
+                ", support=" + support +
+                ", shareStatement=" + shareStatement +
+                ", commentabled=" + commentabled +
+                ", published=" + published +
+                ", recommend=" + recommend +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                '}';
+    }
+}
