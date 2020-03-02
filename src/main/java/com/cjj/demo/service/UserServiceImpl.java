b4b7f0aabebfa8a193c19a73aeaf403ebba40791
jiajunchang <jiajunc@uci.edu>
2020-03-02 06:08:38 -0800

    Add md5 encode

--- src/main/java/com/cjj/demo/service/UserServiceImpl.java
+++ src/main/java/com/cjj/demo/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.cjj.demo.service;
 
 import com.cjj.demo.dao.UserRepository;
 import com.cjj.demo.po.User;
+import com.cjj.demo.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User checkUser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
+        User user = userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
         return user;
     }
 }
