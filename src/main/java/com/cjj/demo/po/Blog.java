c2ae854567beecef37471351c7c1fea45315e604
jiajunchang <jiajunc@uci.edu>
2020-03-02 04:47:48 -0800

    Add Type/Tag/Comment/User.java

--- src/main/java/com/cjj/demo/po/Blog.java
+++ src/main/java/com/cjj/demo/po/Blog.java
@@ -1,10 +1,9 @@
 package com.cjj.demo.po;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "t_blog")
@@ -23,8 +22,22 @@ public class Blog {
     private boolean commentable;
     private boolean published;
     private boolean recommend;
+    @Temporal(TemporalType.TIMESTAMP)
     private Date createTime;
+    @Temporal(TemporalType.TIMESTAMP)
     private Date updateTime;
+
+    @ManyToOne
+    private Type type;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST})
+    private List<Tag> tags = new ArrayList<>();
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "blog")
+    private List<Comment> comments = new ArrayList<>();
 
     public Blog() {
     }
@@ -33,52 +46,136 @@ public class Blog {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getContent() {
         return content;
     }
 
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     public String getFirstPicture() {
         return firstPicture;
+    }
+
+    public void setFirstPicture(String firstPicture) {
+        this.firstPicture = firstPicture;
     }
 
     public String getFlag() {
         return flag;
     }
 
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
     public Integer getViews() {
         return views;
+    }
+
+    public void setViews(Integer views) {
+        this.views = views;
     }
 
     public boolean isSupport() {
         return support;
     }
 
+    public void setSupport(boolean support) {
+        this.support = support;
+    }
+
     public boolean isShareStatement() {
         return shareStatement;
+    }
+
+    public void setShareStatement(boolean shareStatement) {
+        this.shareStatement = shareStatement;
     }
 
     public boolean isCommentable() {
         return commentable;
     }
 
+    public void setCommentable(boolean commentable) {
+        this.commentable = commentable;
+    }
+
     public boolean isPublished() {
         return published;
+    }
+
+    public void setPublished(boolean published) {
+        this.published = published;
     }
 
     public boolean isRecommend() {
         return recommend;
     }
 
+    public void setRecommend(boolean recommend) {
+        this.recommend = recommend;
+    }
+
     public Date getCreateTime() {
         return createTime;
     }
 
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
     public Date getUpdateTime() {
         return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
     }
 
     @Override
@@ -92,7 +189,7 @@ public class Blog {
                 ", views=" + views +
                 ", support=" + support +
                 ", shareStatement=" + shareStatement +
-                ", commentabled=" + commentabled +
+                ", commentable=" + commentable +
                 ", published=" + published +
                 ", recommend=" + recommend +
                 ", createTime=" + createTime +
