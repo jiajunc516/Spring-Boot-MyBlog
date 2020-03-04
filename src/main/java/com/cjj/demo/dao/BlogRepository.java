f77c0d3cd7423cb8ad6a9b72007c40a98e93c064
jiajunchang <jiajunc@uci.edu>
2020-03-04 06:01:01 -0800

    Map data to index page

--- src/main/java/com/cjj/demo/dao/BlogRepository.java
+++ src/main/java/com/cjj/demo/dao/BlogRepository.java
@@ -1,8 +1,15 @@
 package com.cjj.demo.dao;
 
 import com.cjj.demo.po.Blog;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BlogRepository extends JpaRepository<Blog, Long>, JpaSpecificationExecutor<Blog> {
+
+    @Query("select b from Blog b where b.recommend = true")
+    List<Blog> findTop(Pageable pageable);
 }
--- src/main/java/com/cjj/demo/web/IndexController.java
+++ src/main/java/com/cjj/demo/web/IndexController.java
@@ -1,24 +1,40 @@
 package com.cjj.demo.web;
 
 import com.cjj.demo.NotFoundException;
+import com.cjj.demo.service.BlogService;
+import com.cjj.demo.service.TagService;
+import com.cjj.demo.service.TypeService;
+import com.cjj.demo.vo.BlogQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private BlogService blogService;
+    @Autowired
+    private TypeService typeService;
+    @Autowired
+    private TagService tagService;
+
     @RequestMapping("/")
-    public String index() {
-//        int i = 9/0;
-//        String blog = null;
-//        if (blog == null) {
-//            throw new NotFoundException("博客不存在");
-//        }
+    public String index(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+                        BlogQuery blog, Model model) {
+        model.addAttribute("page", blogService.listBlog(pageable));
+        model.addAttribute("types", typeService.listTypeTop(6));
+        model.addAttribute("tags", tagService.listTagTop(10));
+        model.addAttribute("recommendBlogs", blogService.listRecommendBlogTop(8));
         return "index";
     }
 
-    @RequestMapping("/blog")
+    @RequestMapping("/blog/{id}")
     public String blog() {
         return "blog";
     }
